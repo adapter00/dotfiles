48e6fcdca9e014be84461847e43f1686c8960bfe
adapter00 <takao19880607@gmail.com>
2014-04-05 12:16:09 +0900

    create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!bin/sh
+
+
+
+DOTPATH = ${HOME}/.dotfiles
+
+ln -s $DOTAPATH/.vimrc ~/.vimrc
+ln -s $DOTAPATH/.gvimrc ~/.gvimrc
+ln -s $DOTPATH/.zshrc ~/.zhsrc
+ln -s $DOTPATH/.vim/ ~/.vim
+ln -s $DOTPATH/.dictionary ~/.dictionary
+ln -s $DOTPATH/.templete ~/.templete
+ln -s $DOTPATH/.tmux ~/.tmux
+
+source ~/.zshrc
+
+
+
