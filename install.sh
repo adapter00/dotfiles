78f196665215772ba407e5190960d0fd74bf6b76
adapter00 <takao19880607@gmail.com>
2014-04-16 00:17:27 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@
 DOTPATH = ${HOME}/.dotfiles
 
 ln -s $DOTPATH/.vimrc $HOME/.vimrc
-ln -s $DOTPATH/.gvimrc ~/.gvimrc
-ln -s $DOTPATH/.zshrc $HOME~/.zshrc
+ln -s $DOTPATH/.gvimrc $HOME/.gvimrc
+ln -s $DOTPATH/.zshrc $HOME/.zshrc
 ln -s $DOTPATH/.vim/ $HOME/.vim
 ln -s $DOTPATH/.dictionary $HOME/.dictionary
 ln -s $DOTPATH/.templete $HOME/.templete
-ln -s $DOTPATH/.tmux ~/.tmux
+ln -s $DOTPATH/.tmux $HOME/.tmux
 
 source ~/.zshrc
 
