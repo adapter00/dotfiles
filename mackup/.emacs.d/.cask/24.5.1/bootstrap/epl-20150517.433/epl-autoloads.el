39f0a5890e6b08307c0f524b02b506b53fd22b34
adapter00 <adapter00@gmail.com>
2017-11-23 15:17:15 +0900

    remove mackup file

--- mackup/.emacs.d/.cask/24.5.1/bootstrap/epl-20150517.433/epl-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("epl.el") (22049 51843 977156 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epl-autoloads.el ends here
