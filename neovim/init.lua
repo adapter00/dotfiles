54556c052d4aa8caec85df1d46a32df193831d51
adapter00 <3038531+adapter00@users.noreply.github.com>
2026-02-28 12:03:13 +0900

    feat: replace init.vim entrypoint with init.lua + lazy.nvim bootstrap

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,5 +1,31 @@
-require('settings') 
-require('plugins')
-require('dap-go').setup()
-vim.api.nvim_out_write("init.lua ha been loaded\n")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
+-- Load core config
+require("settings")
+require("autocmds")
+require("keymaps")
+
+-- Load plugins (lazy.nvim)
+require("plugins")
+
+-- Load LSP
+require("lsp")
+
+-- Load extra settings ($EXTRA_VIM / .vimrc.local)
+require("extra_settings")
+
+-- Machine-local overrides (not tracked)
+local local_init = vim.fn.stdpath("config") .. "/_init.lua"
+if vim.loop.fs_stat(local_init) then
+  dofile(local_init)
+end
